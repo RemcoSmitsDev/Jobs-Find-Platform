022002878f7213ad0d54a8e6b1a2eaa9e1334a6b
carpiee <62463826+carpiee@users.noreply.github.com>
2020-06-16 20:36:29 +0200

    job page for mibile ui changes

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -1,15 +1,16 @@
 @extends ('layout.app')
 @section ('content')
 
-<div class="px-6 space-y-4 container mx-auto max-w-screen-lg flex flex-wrap-reverse justify-center">
+<div class="px-6 container mx-auto max-w-screen-lg flex flex-wrap-reverse justify-center">
     <div class="lg:mt-0 mt-12 lg:w-1/4 w-full lg:block flex">
-        <div class="hidden lg:block mb-4 h-12 w-12 lg:h-24 lg:w-24 rounded-full overflow-hidden shadow flex-shrink-0">
+        <div
+            class="hidden lg:block ml-8 mb-4 h-12 w-12 lg:h-24 lg:w-24 rounded-full overflow-hidden shadow flex-shrink-0">
             <img class="h-24 w-auto"
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Square_-_black_simple.svg/500px-Square_-_black_simple.svg.png"
                 alt="">
         </div>
         <div class="w-full space-y-2 flex-shrink-0">
-            <p class="lg:mb-0 mb-8 pb-2 font-bold text-lg md:border-0 border-b lg:text-left text-center">Extra
+            <p class="lg:mb-0 mb-8 pb-2 font-bold text-lg md:border-0 border-b md:text-left text-center">Extra
                 information</p>
             <p class="font-semibold">Salary
                 <span class="text-sm font-semibold">
@@ -36,8 +37,8 @@
         </div>
     </div>
     <div class="lg:mr-10 max-w-2xl">
-        <p class="pb-2 mb-8 font-semibold text-xl text-center border-b">{{ $job->title }} by
-            {{ $job->company_name }}
+        <p class="pb-2 lg:mb-8 font-semibold text-2xl lg:text-center lg:border-b">
+            {{ $job->title }}
         </p>
         <p class="text-sm leading-relaxed">{{ $job->discription }}</p>
     </div>
