43048c3f4e857e3d18c1a89c44f240d6676b3117
carpiee <62463826+carpiee@users.noreply.github.com>
2020-06-16 22:29:03 +0200

    fixed nav search bar now in the center

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,26 +8,50 @@
 
     <body>
         <div class="fixed top-0 left-0 w-full bg-black">
-            <div class="px-6 py-2 w-full inline-flex items-center justify-between">
-                <div class="text-white">
+            <div class="px-6 pt-4 -mt-px w-full flex items-center">
+                <div class="hidden text-white">
                     <a href="{{ route('AllJobs') }}">Mij applies</a>
                 </div>
-                <form class="w-auto" action="/search" method="post">
-                    {{ csrf_field() }}
-                    <div class="flex">
-                        <input class="w-full max-w-md px-4 py-2 rounded-l" type="search" id="q" name="q"
+                <div class="mx-auto">
+                    <form class="flex items-center" action="/search" method="post">
+                        {{ csrf_field() }}
+                        <input class="w-64 max-w-md px-4 py-2 rounded-l" type="search" name="q"
                             placeholder="html, css, front-end, backend">
-                        <button class="px-4 py-2 bg-indigo-400 text-white font-semibold" type="submit">Search</button>
-                    </div>
-                </form>
-                <div class="text-white">
+                        <button class="px-4 py-2 bg-indigo-400 text-white font-semibold rounded-r"
+                            type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="hidden text-white">
                     <a href="{{ route('AllJobs') }}">Mij applies</a>
                 </div>
             </div>
         </div>
-        <div class="my-20">
+
+        @if(!empty($errorMessage))
+        <div class="fixed bottom-0 right-0 w-full max-w-md bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert" id="fade-message">
+            <strong class="font-bold">Holy smokes!</strong>
+            <span class="block sm:inline">{{ $errorMessage }}</span>
+            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20">
+                    <title>Close</title>
+                    <path
+                        d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+            </span>
+        </div>
+        @endif
+        <div class="mt-24 mb-20">
             @yield('content')
         </div>
+        <script>
+        $(function() {
+            setTimeout(function() {
+                $('.fade-message').slideUp();
+            }, 5000);
+        });
+        </script>
     </body>
 
 </html>
